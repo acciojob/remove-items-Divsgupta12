3172d6f60bf20f3a8f33a295763f1d79230b2ad8
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-05-08 15:46:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+const selectinput = document.querySelector("#colorSelect");
+const btn = document.querySelector("#btn");
+btn.addEventListener("click",()=>selectinput.remove(selectinput.selectedIndex));
